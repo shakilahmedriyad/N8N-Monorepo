bd41389c69855164c390aef1aa03641cf6a579ef
shakilahmedriyad <riyad.ahmed1886@gmail.com>
2026-06-15 08:20:20 +0600

    fully setup the better-auth

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: false,
   });
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
