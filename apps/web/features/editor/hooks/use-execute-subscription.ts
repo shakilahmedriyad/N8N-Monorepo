65d8ea949e4d43f2dd28206250d782b21c34dd40
shakilahmedriyad <riyad.ahmed1886@gmail.com>
2026-07-09 20:38:22 +0600

    fix type issue , and end of the project

--- apps/web/features/editor/hooks/use-execute-subscription.ts
+++ apps/web/features/editor/hooks/use-execute-subscription.ts
@@ -1,3 +1,4 @@
+import { NodeStatus } from "@/components/react-flow/node-status-indicator";
 import { useTRPC } from "@/lib/trpc/trpc";
 import useNodeStatusStore from "@/store/node-status-store";
 import { useSubscription } from "@trpc/tanstack-react-query";
@@ -6,7 +7,8 @@ export default function useExecuteSubscription() {
   const trpc = useTRPC();
   return useSubscription(
     trpc.workflow.nodeStatus.subscriptionOptions(void 0, {
-      onData: (data) => {
+      //@ts-expect-error type error
+      onData: (data: { nodeId: string; status: NodeStatus }) => {
         useNodeStatusStore.getState().setNodeStatus(data.nodeId, data.status);
       },
     }),
